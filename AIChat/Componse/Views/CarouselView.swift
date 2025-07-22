4ebddd6eae0b7a6f69e56c71a6dffb9ac90bf65e
sinduke <sinduke@outlook.com>
2025-07-22 11:25:34 +0800

    [功能] 1.添加 HeroSection的滚动动画

--- AIChat/Componse/Views/CarouselView.swift
+++ AIChat/Componse/Views/CarouselView.swift
@@ -17,9 +17,22 @@ struct CarouselView<Content: View, T: Hashable >: View {
                 LazyHStack(spacing: 0) {
                     ForEach(items, id: \.self) { item in
                         content(item)
-                            .scrollTransition(.interactive.threshold(.visible(0.95)), axis: .horizontal, transition: { content, phase in
-                                content
-                                    .scaleEffect(phase.isIdentity ? 1 : 0.9)
+                            .scrollTransition(
+                                .interactive
+                                    .animation(.easeInOut(duration: 0.25))
+                                    .threshold(
+                                    .visible(
+                                        0.95
+                                    )
+                                ),
+                                axis: .horizontal,
+                                transition: {
+                                    content,
+                                    phase in
+                                    content
+                                        .scaleEffect(
+                                            phase.isIdentity ? 1 : 0.9
+                                        )
                             })
                             .containerRelativeFrame(.horizontal, alignment: .center)
                             .id(item)
@@ -30,7 +43,7 @@ struct CarouselView<Content: View, T: Hashable >: View {
             .scrollIndicators(.hidden)
             .scrollTargetLayout()
             .scrollTargetBehavior(.paging)
-            .scrollPosition(id: $selection)
+            .scrollPosition(id: $selection, anchor: .center)
             .onChange(of: items.count, { _, _ in
                 updateSelectionIfNeeded()
             })
@@ -42,7 +55,7 @@ struct CarouselView<Content: View, T: Hashable >: View {
                 ForEach(items, id: \.self) { item in
                     Circle()
                         .fill(item == selection ? .accent: .secondary.opacity(0.5))
-                        .frame(height: 8)
+                        .frame(width: 8, height: 8)
                 }
             }
             .animation(.linear, value: selection)
