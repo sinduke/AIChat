bae3f639febecbc0031e8f307e3da8d970161269
sinduke <sinduke@outlook.com>
2025-07-29 02:56:16 +0800

    [功能] 1.添加Firebase

--- AIChat/Core/AIChatApp.swift
+++ AIChat/Core/AIChatApp.swift
@@ -6,9 +6,18 @@
 //
 
 import SwiftUI
+import Firebase
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions languageOption: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 struct AIChatApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             AppView()
